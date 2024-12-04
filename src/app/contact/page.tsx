ba7bfeb62bdba39ecf217f53a98574943771d080
md-parves <parves.dev@gmail.com>
2024-12-04 13:26:15 +0600

    started to contactus page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,30 @@
+import { Navbar } from "@/components/layout/Header";
+
+function page() {
+  return (
+    <>
+      <Navbar />
+      <section>
+        <div className="bg-[url('/images/demo-beauty-salon-title-bg.jpg')] bg-no-repeat bg-cover bg-center flex justify-center items-center h-[300px] text-white text-3xl">
+          <h2>Contact us</h2>
+        </div>
+
+        <div>
+
+             
+
+
+
+
+
+
+
+
+
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default page;
